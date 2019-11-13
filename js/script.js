08c0ba98f7e5902e9b394af7965faae2348236be
Paula <paulaklimas@Paula-Klimas-Macbook-Pro.local>
2019-11-13 00:37:12 -0700

    adds changes to header, navigation, and projects page

--- js/script.js
+++ js/script.js
@@ -8,35 +8,35 @@ function revealMessage() {
   }
 }
 
-function clockBattery() {
-  // calculating time using JS built-in Date object
-  var current = new Date();
+// function clockBattery() {
+//   // calculating time using JS built-in Date object
+//   var current = new Date();
 
-  // modulo makes a 12-hour clock for face
-  var hr = current.getHours() % 12
-  var min = current.getMinutes()
-  var sec = current.getSeconds()
-  var milli = current.getMilliseconds()
+//   // modulo makes a 12-hour clock for face
+//   var hr = current.getHours() % 12
+//   var min = current.getMinutes()
+//   var sec = current.getSeconds()
+//   var milli = current.getMilliseconds()
 
-  var clock = document.querySelector(".clock")
-  var hourHand = clock.querySelector(".hand.hour")
-  var minHand = clock.querySelector(".hand.minute")
-  var secHand = clock.querySelector(".hand.second")
+//   var clock = document.querySelector(".clock")
+//   var hourHand = clock.querySelector(".hand.hour")
+//   var minHand = clock.querySelector(".hand.minute")
+//   var secHand = clock.querySelector(".hand.second")
 
-  // each hour is separated by 30 degrees
-  var hourRotation = 30 * hr + (0.5 * min);
-  // each minute is 
-  var minuteRotation = 6 * min + (0.1 * sec);
-  // six degrees in second, ms on top if we want smooth transition of secondhand
-  var secRotation = 6 * sec + 0.006 * milli;
+//   // each hour is separated by 30 degrees
+//   var hourRotation = 30 * hr + (0.5 * min);
+//   // each minute is 
+//   var minuteRotation = 6 * min + (0.1 * sec);
+//   // six degrees in second, ms on top if we want smooth transition of secondhand
+//   var secRotation = 6 * sec + 0.006 * milli;
 
-  hourHand.style.transform = 'rotate(' + hourRotation + 'deg)'
-  minHand.style.transform = "rotate(" + minuteRotation + "deg)"
-  secHand.style.transform = "rotate(" + secRotation + "deg)"
+//   hourHand.style.transform = 'rotate(' + hourRotation + 'deg)'
+//   minHand.style.transform = "rotate(" + minuteRotation + "deg)"
+//   secHand.style.transform = "rotate(" + secRotation + "deg)"
 
-  // runs every single frame
-  requestAnimationFrame(clockBattery);
-}
+//   // runs every single frame
+//   requestAnimationFrame(clockBattery);
+// }
 
-// run the clock on load and during page, basically always
-clockBattery();
+// // run the clock on load and during page, basically always
+// clockBattery();
